3aee376cc4ba2c221383ab66f273599a0c487c3d
udit <yudistrange@gmail.com>
2023-01-25 13:41:46 +0530

    Accept commandline args in loxrs

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,9 @@
 fn main() {
-    println!("Hello World!")
+    let args = std::env::args();
+    let args_len = args.count();
+    match args_len {
+        1 => println!("Starting lox interpreter..."), // `loxrs` is the always the first argument
+        2 => println!("Compiling"),                   // accept one script
+        _ => println!("Usage: loxrs [script]"),       // ignore more than 1 argument
+    }
 }
